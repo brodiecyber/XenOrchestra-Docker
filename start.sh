04f56fab7993292fc75628c2fae4ddf5bf1ed030
brodie petersen <94775329+brodiecyber@users.noreply.github.com>
2023-11-08 16:15:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@
 redis-server &
 
 # Start Xen Orchestra
-yarn --cwd /xen-orchestra/packages/xo-server start
+#yarn --cwd /xen-orchestra/packages/xo-server start
+cd /xen-orchestra/packages/xo-server/ &&
+service orchestra start
 
 # Keep the script running to keep the container alive
 tail -f /dev/null
