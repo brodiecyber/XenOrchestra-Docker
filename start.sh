b785e15f2504ac5222f443369ecb5d041ca92155
brodie petersen <94775329+brodiecyber@users.noreply.github.com>
2023-11-09 15:20:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+# Disbale memry overcommit
+sysctl vm.overcommit_memory=1 &&
+
+# Set clock sources
+echo xen > /sys/devices/system/clocksource/clocksource0/current_clocksource &&
+
+# Disable Transparent Huge Pages
+echo madvise > /sys/kernel/mm/transparent_hugepage/enabled &&
+
 # Start Redis server
 redis-server &
 
 # Start Xen Orchestra
 #yarn --cwd /xen-orchestra/packages/xo-server start
+
 cd /xen-orchestra/packages/xo-server/ &&
 service orchestra start
 
